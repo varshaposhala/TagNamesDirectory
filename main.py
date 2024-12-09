cea82289ee12d35a668299050f817cdc74b98306
Ravikant <ravikantraipuria1357@gmail.com>
2024-12-09 12:45:28 +0530

    added favicon

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from streamlit import columns
-
+import os
 import json_utils
-
 import streamlit as st
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
+
+# Change default theme or settings dynamically
+st.set_page_config(
+    page_title="TOPIN TAGS | PST-Tech",
+    page_icon="icon.png",
+    layout="wide",
+)
 
 data = None
 
 if not data:
     data = json_utils.get_json_data()
 
-st.markdown('# Find Topin Tags')
+st.markdown(
+    """
+    <style>
+    .center-text {
+        text-align: center;
+    }
+    </style>
+    <div class="center-text">
+        <h1>Find Topin Tags</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 col1, col2, col3 = st.columns([1,1,1])
 
