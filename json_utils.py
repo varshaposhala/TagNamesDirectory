0375c39dfb7dc74c2e16dfd821a310165d96f93e
Ravikant <ravikantraipuria1357@gmail.com>
2025-01-08 15:29:15 +0530

    Ui is updated from columns to linear top to bottom

--- json_utils.py
+++ json_utils.py
@@ -1,5 +1,6 @@
 import os
 import json
+from idlelib.iomenu import encoding
 
 
 def get_json_data():
@@ -29,6 +30,27 @@ def get_sections(data):
 def get_topics(data, section):
     if section in data.keys():
         return sorted(data[section].keys())
+
+
+def get_topic_value(section, topic_value):
+    file_path = os.path.join(os.getcwd(), 'jsons', 'tags.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        tags_dict = json.load(f)
+        for topic in tags_dict.get(section):
+            if topic.get('topic_name').get('value') == topic_value:
+                return topic.get('topic_name').get('label')
+
+
+def get_sub_topic_value(section, topic_value, sub_topic_value):
+    file_path = os.path.join(os.getcwd(), 'jsons', 'tags.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        tags_dict = json.load(f)
+        for topic in tags_dict.get(section):
+            if topic.get('topic_name').get('value') == topic_value:
+                for sub_topic in topic.get('sub_topics'):
+                    if sub_topic.get('sub_topic_name').get('value') == sub_topic_value:
+                        return sub_topic.get('sub_topic_name').get('label')
+
 
 def get_sub_topics(data, section, topic):
     if section in data.keys() and topic in data[section].keys():
